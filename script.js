1986641470eeffdfeee15689b31447f5b1cb0dc4
Dawid108 <gustowskidawid108@gmail.com>
2025-06-16 10:52:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,10 +13,3 @@ function numerateTables() {
 window.onload = function() {
     numerateTables();
 };
-
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function() {
-        document.querySelectorAll('.nav-link').forEach(el => el.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
