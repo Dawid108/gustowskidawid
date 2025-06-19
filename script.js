d49623863254f614bd5fc9be05416b9f9f12777f
Dawid108 <gustowskidawid108@gmail.com>
2025-06-19 11:25:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function numerateTables() {
-    document.querySelectorAll("table").forEach(table => {
+    // Wybieramy wszystkie tabele, które NIE mają klasy 'nienumerowana'
+    document.querySelectorAll("table:not(.nienumerowana)").forEach(table => {
         let rows = table.querySelectorAll("tr:not(:first-child)");
         rows.forEach((row, index) => {
             let firstCell = row.querySelector("td:first-child");
-            if (firstCell) {firstCell.textContent = index + 1;
+            if (firstCell) {
+                firstCell.textContent = index + 1;
             }
         });
     });
