1e3152124d1458fb7037b6c666975796153dc283
Dawid108 <gustowskidawid108@gmail.com>
2026-04-08 15:14:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const fileName = window.location.pathname.split("/").pop() || "index.html";
+const pageUrl = window.location.href;
+
+let namePart = fileName.includes("_")
+    ? fileName.split("_")[1].replace(".html", "")
+    : fileName.replace(".html", "");
+
+const now = new Date();
+const dateString = now.toLocaleDateString("pl-PL");
+
+let historia108 = JSON.parse(localStorage.getItem("historia108")) || [];
+
+historia108 = historia108.filter(item => item.file !== fileName);
+
+historia108.push({
+    file: fileName,
+    name: namePart,
+    url: pageUrl,
+    date: dateString
+});
+
+if (historia108.length > 100) {
+    historia108 = historia108.slice(historia108.length - 100);
+}
+
+localStorage.setItem("historia108", JSON.stringify(historia108));
+
+
 function numerateTables() {
     // Wybieramy wszystkie tabele, które NIE mają klasy 'nienumerowana'
     document.querySelectorAll("table:not(.nienumerowana)").forEach(table => {
